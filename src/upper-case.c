7ff25cd48385ce1cc0b0882f294057c96548ce2e
Ankit Pati <contact@ankitpati.in>
2017-05-27 18:10:35 +0530

    upper casing helper

---
+++ src/upper-case.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+char *getstr()
+{
+    int c;
+    size_t n;
+    char *s, *t;
+    if(!(s = t = malloc(1))) return NULL;
+    for(n=0; (c=getchar())!=EOF && c-'\n' && (t=realloc(s, n+2)); s[n++]=c) s=t;
+    if(!t){
+        free(s);
+        return NULL;
+    }
+    s[n] = '\0';
+    return s;
+}
+
+int main()
+{
+    char *s;
+
+    for (s = getstr(); *s; ++s) putchar(toupper(*s));
+    putchar('\n');
+
+    return 0;
+}
