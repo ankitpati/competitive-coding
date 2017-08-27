66c547a7e9f226fd2c7f9f86e27a463ba813297f
Ankit Pati <contact@ankitpati.in>
2017-08-27 21:49:27 +0530

    remove unnecessary map

--- src/big-sorting.pl
+++ src/big-sorting.pl
@@ -5,8 +5,8 @@ use warnings;
 
 <>;                         # discard n; we are going to look for EOF anyways
 
-print "$_\n" foreach sort {
+print foreach sort {
     my $len = length ($a) - length ($b);
     return $len if $len;    # two-level sort: sort by value only
     return $a cmp $b;       #                 if lengths match.
-} map { chomp $_; $_ } <>;
+} <>;
