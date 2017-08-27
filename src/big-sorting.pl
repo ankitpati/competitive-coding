7302607722956ca0b129c7986608336dd9d40edd
Ankit Pati <contact@ankitpati.in>
2017-08-27 22:05:29 +0530

    Revert "remove unnecessary map"
    Turns out it is actually necessary, because of the bone-headed
    HackerRank implementation that clobbers trailing newlines from
    STDIN, so 10 lines of input is actually 9 lines and a dandling
    set of characters.

--- src/big-sorting.pl
+++ src/big-sorting.pl
@@ -5,8 +5,8 @@ use warnings;
 
 <>;                         # discard n; we are going to look for EOF anyways
 
-print foreach sort {
+print "$_\n" foreach sort {
     my $len = length ($a) - length ($b);
     return $len if $len;    # two-level sort: sort by value only
     return $a cmp $b;       #                 if lengths match.
-} <>;
+} map { chomp $_; $_ } <>;
