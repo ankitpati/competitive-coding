c28b1f260ad1b6ba04c754823397c887a49ded03
Ankit Pati <contact@ankitpati.in>
2017-09-21 21:08:08 +0530

    use dynamic programming to avoid TLE

--- src/coin-change.c
+++ src/coin-change.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+static size_t **hash;
+
 size_t count(size_t n, size_t *c, size_t m)
 {
-    return n ? n < *c ? 0 : m ?
-        count(n, c + 1, m - 1) + count(n - *c, c, m)
-        : 0 : 1;
+    size_t value = hash[m][n];
+    if (value != ~(size_t)0u) return value;
+
+    return hash[m][n] = n ? n < *c ? 0 : m ?
+                            count(n, c + 1, m - 1) + count(n - *c, c, m)
+                            : 0 : 1;
 }
 
 int cmp(const void *a, const void *b)
@@ -15,7 +20,7 @@ int cmp(const void *a, const void *b)
 
 int main()
 {
-    size_t n, m, *c, i;
+    size_t n, m, *c, i, j;
 
     scanf(" %zu %zu%*c", &n, &m);
 
@@ -23,6 +28,13 @@ int main()
     for (i = 0; i < m; ++i) scanf(" %zu%*c", c + i);
 
     qsort(c, m, sizeof(*c), cmp);
+
+    hash = malloc((m + 1) * sizeof(*hash));
+    for (i = 0; i <= m; ++i) {
+        hash[i] = malloc((n + 1) * sizeof(**hash));
+        for (j = 0; j <= n; ++j) hash[i][j] = ~(size_t)0u;
+    }
+
     printf("%zu\n", count(n, c, m));
 
     return 0;
